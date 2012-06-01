9c198540461d8bb755b3645716e6562eef45848a
Nick ODell <nickodell@gmail.com>
2012-06-01 03:21:57 -0600

    Remove unused goertzel algo files

--- gz_build.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup, Extension
- 
-module1 = Extension('gz_dsp', sources = ['gz_dsp.c'])
- 
-setup (name = 'gz_dsp',
-        version = '1.0',
-        description = 'This is a demo package',
-        ext_modules = [module1])
