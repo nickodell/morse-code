377aa35cb45a35f67dfb78c21dd3a708e42ae2a9
Nick ODell <nickodell@gmail.com>
2012-06-08 16:26:55 -0600

    Move some configuration stuff to cfg.py

--- cfg.py
+++ cfg.py
@@ -1,5 +1,12 @@
+### detect_tone.py
 SAMPLE_FREQ = float(44100) #Hz
 MORSE_FREQ = 440 #Hz
 FFT_FREQ = 500 #ideal number of times the gz transform is done a second
 
-OVERLAP_FACTOR = 1.1
+OVERLAP_FACTOR = 1.1 # amount that the spectral transforms overlap
+
+### element_resolve.py
+
+MAX_FUDGE_FACTOR = 0.10 #most that the element timings can be off
+
+EDGE_VARY_PENALTY_FACTOR = 0.50
