006838191b4e6c90f2ccbbd56b2be00249fe70d7
Nick ODell <nickodell@gmail.com>
2012-06-08 16:25:07 -0600

    Avoid Divide-By-Zero

--- detect_tone.py
+++ detect_tone.py
@@ -2,10 +2,12 @@ from gz_dsp import *
 from cfg import *
 
 
+# By FFT, I mean Goertzel transform
 def detect_tone(signal):
 	ideal_samples_per_fft = SAMPLE_FREQ/float(FFT_FREQ)
 	samples_per_cycle = SAMPLE_FREQ/MORSE_FREQ
-	aspf = actual_samples_per_fft = int(samples_per_cycle*round(ideal_samples_per_fft/samples_per_cycle))
+	aspf = actual_samples_per_fft = int(samples_per_cycle*max(round(ideal_samples_per_fft/samples_per_cycle), 1))
+	
 	
 	coeffs = []
 	for i in xrange(0, len(signal), int(aspf/OVERLAP_FACTOR)):
