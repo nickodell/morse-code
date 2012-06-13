a5f34a8011718ba31dc3d70d761bc4583112f133
Nick ODell <nickodell@gmail.com>
2012-06-13 01:44:12 -0600

    Make morse parser not assume that the current working directory is common/

--- common/morse_parse.py
+++ common/morse_parse.py
@@ -1,4 +1,7 @@
-f = open("morse_table.txt")
+import inspect, os
+common_dir = os.path.dirname(inspect.getfile(inspect.currentframe())) # script directory
+
+f = open(os.path.join(common_dir, "morse_table.txt"))
 
 morse_table = f.read()
 morse_table = dict([(morse[0:1], morse[2:len(morse)]) for morse in morse_table.split("\n")])
