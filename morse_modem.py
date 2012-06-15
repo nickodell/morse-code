124489e979ed9d913b97ff688ce65d678579e638
Nick ODell <nickodell@gmail.com>
2012-06-14 18:52:57 -0600

    Add tone generation arguments to gen_tone

--- morse_modem.py
+++ morse_modem.py
@@ -2,13 +2,16 @@ import cProfile
 from demodulate.cfg import *
 from demodulate.detect_tone import *
 from demodulate.element_resolve import *
-from gen_test import *
+from gen_tone import *
+import random
 
 
 
-if __name__ == "__main__":
+if __name__ == "__main__":	
+	WPM = random.uniform(2,20)
+	pattern = [1,0,1,1,1,0,0,0,0,0,0,0] # morse code 'A'
 	#gen_test_data()
-	data = gen_test_data()
+	data = gen_tone(pattern)
 	#print len(data)/SAMPLE_FREQ
 	#cProfile.run('detect_tone(data)')
 	#print detect_tone(data)
